4230cfa76e0576266e3e3399030087a68eb555ac
Jinxin Hu <hujinxin0607@gmail.com>
2022-02-02 01:33:59 -0700

    2-5 修改启动文案-english version

--- src/main/java/com/project/hjxwiki/HjxwikiApplication.java
+++ src/main/java/com/project/hjxwiki/HjxwikiApplication.java
@@ -14,8 +14,8 @@ public class HjxwikiApplication {
     public static void main(String[] args) {
         SpringApplication app = new SpringApplication(HjxwikiApplication.class);
         Environment env = app.run(args).getEnvironment();
-        LOG.info("启动成功！！");
-        LOG.info("地址: \thttp://127.0.0.1:{}", env.getProperty("server.port"));
+        LOG.info("Successful Runing！！");
+        LOG.info("Address: \thttp://127.0.0.1:{}", env.getProperty("server.port"));
     }
 
 }
