41401d9d889b488a5050268edfd7107693968856
Jinxin Hu <hujinxin0607@gmail.com>
2022-02-01 17:33:08 -0700

    2-2 init

---
+++ src/main/java/com/project/hjxwiki/HjxwikiApplication.java
@@ -0,0 +1,13 @@
+package com.project.hjxwiki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HjxwikiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HjxwikiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/hjxwiki/HjxwikiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.hjxwiki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HjxwikiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
