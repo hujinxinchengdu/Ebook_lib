0d8aaadca2284c964f33dcefb3afcdc384d8c22d
Jinxin Hu <hujinxin0607@gmail.com>
2022-02-06 18:06:07 -0700

    3-4 add dependence pom.xml for MyBatis and Mysql. Add dataSource into application.properties for not making error.

--- src/main/java/com/project/hjxwiki/controller/TestController.java
+++ src/main/java/com/project/hjxwiki/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.project.hjxwiki.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,13 +13,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
+    @Value("${test.hello:TEXT}")
+    private String testHello;
+
+
     /**
      * GET, POST, PUT, DELETE
      * @return
      */
     @GetMapping("/hello")
     public String hello() {
-        return "Hello World!";
+        return "Hello World!" + testHello;
     }
 
     @PostMapping("/hello/post")
