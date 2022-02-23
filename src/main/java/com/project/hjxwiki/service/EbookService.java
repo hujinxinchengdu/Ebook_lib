1023cec6b7d46a4c64f67dececcda58fbaa05826
Jinxin Hu <hujinxin0607@gmail.com>
2022-02-23 02:46:10 -0700

    3-8 web layout development

--- src/main/java/com/project/hjxwiki/service/EbookService.java
+++ src/main/java/com/project/hjxwiki/service/EbookService.java
@@ -5,10 +5,11 @@ import com.project.hjxwiki.domain.EbookExample;
 import com.project.hjxwiki.mapper.EbookMapper;
 import com.project.hjxwiki.req.EbookReq;
 import com.project.hjxwiki.resp.EbookResp;
-import com.project.hjxwiki.util.CopyUtil;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -27,16 +28,13 @@ public class EbookService {
         criteria.andNameLike("%" + req.getName() + "%");
         List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
 
-//        List<EbookResp> respList = new ArrayList<>();
-//        for (Ebook ebook : ebookList) {
-////            EbookResp ebookResp = new EbookResp();
-////            BeanUtils.copyProperties(ebook,ebookResp);
-            //对象复制
-//            EbookResp ebookResp = CopyUtil.copy(ebook, EbookResp.class);
-//            respList.add(ebookResp);
-//        }
-        //列表复制, 用CopyUtil来封装BeanUtils操作行为
-        List<EbookResp> list = CopyUtil.copyList(ebookList, EbookResp.class);
-        return list;
+        List<EbookResp> respList = new ArrayList<>();
+        for (Ebook ebook : ebookList) {
+            EbookResp ebookResp = new EbookResp();
+            BeanUtils.copyProperties(ebook,ebookResp);
+            respList.add(ebookResp);
+        }
+
+        return respList;
     }
 }
--- web/src/main.ts
+++ web/src/main.ts
@@ -2,7 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Antd from 'ant-design-vue';
+import 'ant-design-vue/dist/antd.css';
 
 //配置插件,所有插件都需要在这里配置
 //看到#就要想到选择器 vue cli 初始执行main.ts, 将内容页app.vue渲染到index.html,完成页面显示
-createApp(App).use(store).use(router).mount('#app')
+createApp(App).use(store).use(router).use(Antd).mount('#app');
