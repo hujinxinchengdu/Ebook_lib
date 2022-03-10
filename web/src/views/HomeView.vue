119e8757892b9a6b0cc6fc4c6e2c340b6e3b6268
Jinxin Hu <hujinxin0607@gmail.com>
2022-03-09 18:57:40 -0700

    4-7 delete HelloWorld.vue and relative code

--- web/src/views/HomeView.vue
+++ web/src/views/HomeView.vue
@@ -52,15 +52,3 @@
     </a-layout-content>
   </a-layout>
 </template>
-
-<script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
-
-export default defineComponent({
-  name: 'HomeView',
-  components: {
-    HelloWorld,
-  },
-});
-</script>
