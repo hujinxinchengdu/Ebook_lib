df594b8f4201d34231745461f7b7fcab5ea3bd5e
Jinxin Hu <hujinxin0607@gmail.com>
2022-03-09 18:55:36 -0700

    4-7 build the-footer component.

---
+++ web/src/components/the-footer.vue
@@ -0,0 +1,17 @@
+<template>
+  <a-layout-footer style="text-align: center">
+    Knowledge WIKI ©2022 Created by HJX
+  </a-layout-footer>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'the-footer',
+});
+</script>
+
+<style scoped>
+
+</style>
