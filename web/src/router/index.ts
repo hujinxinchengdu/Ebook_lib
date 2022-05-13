2e023ee779500082f96789fb9a57a64683c6d7ab
Jinxin Hu <hujinxin0607@gmail.com>
2022-05-13 15:07:04 -0700

    6-2 design and implement ebook management page new page development: page router list

--- web/src/router/index.ts
+++ web/src/router/index.ts
@@ -1,21 +1,27 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 import HomeView from '../views/home.vue'
 import AboutView from '../views/aboutView.vue'
+import AdminEbook from '../views/admin/admin-ebook.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
-    name: 'home',
+    name: 'Home',
     component: HomeView
   },
   {
     path: '/about',
-    name: 'about',
-    components: AboutView
+    name: 'About',
+    component: AboutView
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     // component: () => import(/* webpackChunkName: "about" */ '../views/aboutView.vue')
+  },
+  {
+    path: '/admin/ebook',
+    name: 'AdminEbook',
+    component: AdminEbook
   }
 ]
 
