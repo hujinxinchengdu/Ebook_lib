85521e9569eb8d67f3e1844c59b1be25f26f2101
Jinxin Hu <hujinxin0607@gmail.com>
2022-05-13 02:09:14 -0700

    5-9 Increase AOP, print interface time-consuming, request parameters and return parameters     remove filter and interceptor

--- src/main/java/com/project/hjxwiki/config/SpringMvcConfig.java
+++ src/main/java/com/project/hjxwiki/config/SpringMvcConfig.java
@@ -1,20 +1,20 @@
-package com.project.hjxwiki.config;
-
-import com.project.hjxwiki.interceptor.LoginInterceptor;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import javax.annotation.Resource;
-
-@Configuration
-public class SpringMvcConfig implements WebMvcConfigurer {
-
-    @Resource
-    LoginInterceptor loginInterceptor;
-
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(loginInterceptor)
-                .addPathPatterns("/**").excludePathPatterns("/login");
-    }
-}
+//package com.project.hjxwiki.config;
+//
+//import com.project.hjxwiki.interceptor.LoginInterceptor;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//import javax.annotation.Resource;
+//
+//@Configuration
+//public class SpringMvcConfig implements WebMvcConfigurer {
+//
+//    @Resource
+//    LoginInterceptor loginInterceptor;
+//
+//    public void addInterceptors(InterceptorRegistry registry) {
+//        registry.addInterceptor(loginInterceptor)
+//                .addPathPatterns("/**").excludePathPatterns("/login");
+//    }
+//}
