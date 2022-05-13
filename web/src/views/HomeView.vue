a5f9d7084da32105beb82b342d85d8e91f1e8a3b
Jinxin Hu <hujinxin0607@gmail.com>
2022-05-13 01:26:51 -0700

    5-6 Configure axios interceptor to print request parameters and return parameters

--- web/src/views/HomeView.vue
+++ web/src/views/HomeView.vue
@@ -100,18 +100,14 @@ export default defineComponent({
   },
 
   setup() {
-    console.log("setup");
     const ebooks = ref();
     const ebooks1 = reactive({books: []});
 
     onMounted(() => {
-      console.log("onMounted");
-      // axios.get(process.env.VUE_APP_SERVER + "/ebook/list").then((response) => {
       axios.get("/ebook/list").then((response) => {
         const data = response.data;
         ebooks.value = data.content;
         ebooks1.books = data.content;
-        console.log(response);
       });
     });
 
