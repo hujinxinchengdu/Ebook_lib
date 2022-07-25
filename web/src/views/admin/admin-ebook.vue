ddb4c55a100b3200126f3085bce824e42a1b11a2
Jinxin Hu <hujinxin0607@gmail.com>
2022-07-25 15:28:09 -0700

    6-6 E-book management page, front-end and back-end paging integration.

--- web/src/views/admin/admin-ebook.vue
+++ web/src/views/admin/admin-ebook.vue
@@ -43,7 +43,7 @@ import axios from 'axios';
       const ebooks = ref();
       const pagination = ref({
         current: 1,
-        pageSize: 10,
+        pageSize: 4,
         total: 0
       });
       const loading = ref(false);
@@ -86,13 +86,19 @@ import axios from 'axios';
         loading.value = true;
         // 如果不清空现有数据，则编辑保存重新加载数据后，再点编辑，则列表显示的还是编辑前的数据
         ebooks.value = [];
-        axios.get("/ebook/list", params).then((response) => {
+        axios.get("/ebook/list", {
+          params: {
+            page: params.page,
+            size: params.size
+          }
+        }).then((response) => {
           loading.value = false;
           const data = response.data;
           console.log(data)
-          ebooks.value = data.content;
+          ebooks.value = data.content.list;
           //重置分页按钮
           pagination.value.current = params.page;
+          pagination.value.total = data.content.total;
         });
       };
       /**
@@ -107,7 +113,11 @@ import axios from 'axios';
       }
 
       onMounted(() => {
-        handleQuery({});
+        handleQuery({
+          //这里必须和后端PageReq类的参数名字相同, 这样Controller才会将前端的参数自动映射到后端
+          page: 1,
+          size: pagination.value.pageSize
+        });
       });
 
       return {
