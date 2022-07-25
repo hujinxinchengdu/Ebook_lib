0d4c733713ca047811200b98dbf3bc84398044b5
Jinxin Hu <hujinxin0607@gmail.com>
2022-07-25 16:03:26 -0700

    6-6 Home page show up List of E-book by using static page reference, search and show all E-book in one time.

--- web/src/views/home.vue
+++ web/src/views/home.vue
@@ -103,13 +103,21 @@ export default defineComponent({
     const ebooks = ref();
     const ebooks1 = reactive({books: []});
 
+    //开发周期长
     onMounted(() => {
-      axios.get("/ebook/list").then((response) => {
+      axios.get("/ebook/list", {
+        params: {
+          page: 1,
+          size: 1000
+        }
+      }).then((response) => {
         const data = response.data;
-        ebooks.value = data.content;
-        ebooks1.books = data.content;
+        ebooks.value = data.content.list;
+        ebooks1.books = data.content.total;
       });
     });
+
+
 
     const pagination = {
       onChange: (page: any) => {
